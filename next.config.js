105c34ead310f475f4b9beea83d78d69e5300030
George Matali <scholarfile@gmail.com>
2025-08-10 00:31:10 -0400

    chore: align Next.js config for Cloudflare Pages (remove standalone/trailingSlash)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     domains: ['localhost'],
     unoptimized: true,
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
